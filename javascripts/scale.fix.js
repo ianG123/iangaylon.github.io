48009e804870538db0e966f1319c79f5702962c2
ianG123 <54581036+ianG123@users.noreply.github.com>
2019-11-02 19:17:39 +0800

    Update scale.fix.js

--- javascripts/scale.fix.js
+++ javascripts/scale.fix.js
@@ -15,20 +15,3 @@ function gestureStart() {
     }
   }
 }
-
-$(document).ready(function(){
-
-      startAnimation();
-
-
-
-     function startAnimation(){
-      jQuery('.skills').each(function(){
-
-        jQuery(this).find('.skillbar').animate({
-          width:jQuery(this).attr('data-percent')
-        },6000);
-
-    });
-     }
-});
